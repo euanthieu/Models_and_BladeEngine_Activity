7a209a24540138b9252e8cfd26d57b4fd46cae30
euanthieu <euanthieu@gmail.com>
2024-07-27 16:54:43 +0800

    Create factories BookFactory.php

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Book::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'isbn' => $this->faker->isbn13,
+            'title' => $this->faker->sentence,
+            'author' => $this->faker->name,
+            'description' => $this->faker->paragraph,
+            'date_published' => $this->faker->date,
+        ];
+    }
+}
