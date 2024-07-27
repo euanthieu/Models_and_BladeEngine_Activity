9e63bbe9298cb3f8091f5836a7c45e11d3a41b06
euanthieu <euanthieu@gmail.com>
2024-07-27 18:35:41 +0800

    change route to / from /books

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/', [BookController::class, 'index'])->name('books.index');
